0e7ea9458febc3aefdbdd1ae0597189eb89a270d
steve mats mats <steve@matsmats.com>
2024-10-22 10:44:55 -0700

    set failures to get debug into to warning

--- smarttub2influx.py
+++ smarttub2influx.py
@@ -237,7 +237,7 @@ async def info_command(spas, args):
         try:
             debug_status = await spa.get_debug_status()
         except Exception as e:
-            logging.error(e)
+            logging.warning(e)
             return ()
 
         if args.all or args.debug:
